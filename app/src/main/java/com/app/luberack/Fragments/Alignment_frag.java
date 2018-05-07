d4113e738c68e204ff78c3a9b613a567eb93faae
Ahmad_Javid321 <ahmadjavid143@live.com>
2018-05-07 16:13:25 +0500

    Initial

---
+++ app/src/main/java/com/app/luberack/Fragments/Alignment_frag.java
@@ -0,0 +1,24 @@
+package com.app.luberack.Fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.app.luberack.R;
+
+/**
+ * Created by ahmad on 5/7/2018.
+ */
+
+public class Alignment_frag extends Fragment {
+    @Override
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view= inflater.inflate(R.layout.fragment_home_oil_brackes, container, false);
+        return  view;
+    }
+}
