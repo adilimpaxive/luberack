b1ca64d2bdae4014326a541b4fd1c1e94f63849e
Farid Mushtaq <faridmushtaq60@gmail.com>
2018-05-10 16:07:26 +0500

    Initial

--- app/build/generated/source/r/debug/android/support/v7/recyclerview/R.java
+++ app/build/generated/source/r/debug/android/support/v7/recyclerview/R.java
@@ -65,18 +65,18 @@ public final class R {
         public static final int notification_top_pad_large_text = 0x7f0700b7;
     }
     public static final class drawable {
-        public static final int notification_action_background = 0x7f08016f;
-        public static final int notification_bg = 0x7f080170;
-        public static final int notification_bg_low = 0x7f080171;
-        public static final int notification_bg_low_normal = 0x7f080172;
-        public static final int notification_bg_low_pressed = 0x7f080173;
-        public static final int notification_bg_normal = 0x7f080174;
-        public static final int notification_bg_normal_pressed = 0x7f080175;
-        public static final int notification_icon_background = 0x7f080176;
-        public static final int notification_template_icon_bg = 0x7f080177;
-        public static final int notification_template_icon_low_bg = 0x7f080178;
-        public static final int notification_tile_bg = 0x7f080179;
-        public static final int notify_panel_notification_icon_bg = 0x7f08017a;
+        public static final int notification_action_background = 0x7f08016e;
+        public static final int notification_bg = 0x7f08016f;
+        public static final int notification_bg_low = 0x7f080170;
+        public static final int notification_bg_low_normal = 0x7f080171;
+        public static final int notification_bg_low_pressed = 0x7f080172;
+        public static final int notification_bg_normal = 0x7f080173;
+        public static final int notification_bg_normal_pressed = 0x7f080174;
+        public static final int notification_icon_background = 0x7f080175;
+        public static final int notification_template_icon_bg = 0x7f080176;
+        public static final int notification_template_icon_low_bg = 0x7f080177;
+        public static final int notification_tile_bg = 0x7f080178;
+        public static final int notify_panel_notification_icon_bg = 0x7f080179;
     }
     public static final class id {
         public static final int action_container = 0x7f090010;
@@ -121,13 +121,13 @@ public final class R {
         public static final int status_bar_notification_info_overflow = 0x7f0f0095;
     }
     public static final class style {
-        public static final int TextAppearance_Compat_Notification = 0x7f1000ff;
-        public static final int TextAppearance_Compat_Notification_Info = 0x7f100100;
-        public static final int TextAppearance_Compat_Notification_Line2 = 0x7f100102;
-        public static final int TextAppearance_Compat_Notification_Time = 0x7f100105;
-        public static final int TextAppearance_Compat_Notification_Title = 0x7f100107;
-        public static final int Widget_Compat_NotificationActionContainer = 0x7f10018d;
-        public static final int Widget_Compat_NotificationActionText = 0x7f10018e;
+        public static final int TextAppearance_Compat_Notification = 0x7f100100;
+        public static final int TextAppearance_Compat_Notification_Info = 0x7f100101;
+        public static final int TextAppearance_Compat_Notification_Line2 = 0x7f100103;
+        public static final int TextAppearance_Compat_Notification_Time = 0x7f100106;
+        public static final int TextAppearance_Compat_Notification_Title = 0x7f100108;
+        public static final int Widget_Compat_NotificationActionContainer = 0x7f10018e;
+        public static final int Widget_Compat_NotificationActionText = 0x7f10018f;
     }
     public static final class styleable {
         public static final int[] FontFamily = { 0x7f0400d8, 0x7f0400d9, 0x7f0400da, 0x7f0400db, 0x7f0400dc, 0x7f0400dd };
