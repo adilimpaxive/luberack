9dcec7f512d1b5f30a8b02d9b83becafc9a15ce4
Ahmad_Javid321 <ahmadjavid143@live.com>
2018-05-10 16:08:55 +0500

    Initial

--- app/build/generated/source/r/debug/android/support/graphics/drawable/R.java
+++ app/build/generated/source/r/debug/android/support/graphics/drawable/R.java
@@ -50,18 +50,18 @@ public final class R {
         public static final int notification_top_pad_large_text = 0x7f0700b7;
     }
     public static final class drawable {
-        public static final int notification_action_background = 0x7f080173;
-        public static final int notification_bg = 0x7f080174;
-        public static final int notification_bg_low = 0x7f080175;
-        public static final int notification_bg_low_normal = 0x7f080176;
-        public static final int notification_bg_low_pressed = 0x7f080177;
-        public static final int notification_bg_normal = 0x7f080178;
-        public static final int notification_bg_normal_pressed = 0x7f080179;
-        public static final int notification_icon_background = 0x7f08017a;
-        public static final int notification_template_icon_bg = 0x7f08017b;
-        public static final int notification_template_icon_low_bg = 0x7f08017c;
-        public static final int notification_tile_bg = 0x7f08017d;
-        public static final int notify_panel_notification_icon_bg = 0x7f08017e;
+        public static final int notification_action_background = 0x7f080174;
+        public static final int notification_bg = 0x7f080175;
+        public static final int notification_bg_low = 0x7f080176;
+        public static final int notification_bg_low_normal = 0x7f080177;
+        public static final int notification_bg_low_pressed = 0x7f080178;
+        public static final int notification_bg_normal = 0x7f080179;
+        public static final int notification_bg_normal_pressed = 0x7f08017a;
+        public static final int notification_icon_background = 0x7f08017b;
+        public static final int notification_template_icon_bg = 0x7f08017c;
+        public static final int notification_template_icon_low_bg = 0x7f08017d;
+        public static final int notification_tile_bg = 0x7f08017e;
+        public static final int notify_panel_notification_icon_bg = 0x7f08017f;
     }
     public static final class id {
         public static final int action_container = 0x7f090010;
@@ -71,36 +71,36 @@ public final class R {
         public static final int actions = 0x7f09001a;
         public static final int async = 0x7f09002e;
         public static final int blocking = 0x7f090035;
-        public static final int chronometer = 0x7f09005f;
-        public static final int forever = 0x7f0900a7;
-        public static final int icon = 0x7f0900bc;
-        public static final int icon_group = 0x7f0900bd;
-        public static final int info = 0x7f0900c4;
-        public static final int italic = 0x7f0900cc;
-        public static final int line1 = 0x7f0900d4;
-        public static final int line3 = 0x7f0900d5;
-        public static final int normal = 0x7f090117;
-        public static final int notification_background = 0x7f090118;
-        public static final int notification_main_column = 0x7f090119;
-        public static final int notification_main_column_container = 0x7f09011a;
-        public static final int right_icon = 0x7f090138;
-        public static final int right_side = 0x7f090139;
-        public static final int tag_transition_group = 0x7f090171;
-        public static final int text = 0x7f090174;
-        public static final int text2 = 0x7f090176;
-        public static final int time = 0x7f09017f;
-        public static final int title = 0x7f090180;
+        public static final int chronometer = 0x7f090061;
+        public static final int forever = 0x7f0900aa;
+        public static final int icon = 0x7f0900bf;
+        public static final int icon_group = 0x7f0900c0;
+        public static final int info = 0x7f0900c7;
+        public static final int italic = 0x7f0900cf;
+        public static final int line1 = 0x7f0900d7;
+        public static final int line3 = 0x7f0900d8;
+        public static final int normal = 0x7f09011a;
+        public static final int notification_background = 0x7f09011b;
+        public static final int notification_main_column = 0x7f09011c;
+        public static final int notification_main_column_container = 0x7f09011d;
+        public static final int right_icon = 0x7f09013b;
+        public static final int right_side = 0x7f09013c;
+        public static final int tag_transition_group = 0x7f090174;
+        public static final int text = 0x7f090177;
+        public static final int text2 = 0x7f090179;
+        public static final int time = 0x7f090182;
+        public static final int title = 0x7f090183;
     }
     public static final class integer {
         public static final int status_bar_notification_info_maxnum = 0x7f0a000e;
     }
     public static final class layout {
-        public static final int notification_action = 0x7f0c004d;
-        public static final int notification_action_tombstone = 0x7f0c004e;
-        public static final int notification_template_custom_big = 0x7f0c0055;
-        public static final int notification_template_icon_group = 0x7f0c0056;
-        public static final int notification_template_part_chronometer = 0x7f0c005a;
-        public static final int notification_template_part_time = 0x7f0c005b;
+        public static final int notification_action = 0x7f0c004f;
+        public static final int notification_action_tombstone = 0x7f0c0050;
+        public static final int notification_template_custom_big = 0x7f0c0057;
+        public static final int notification_template_icon_group = 0x7f0c0058;
+        public static final int notification_template_part_chronometer = 0x7f0c005c;
+        public static final int notification_template_part_time = 0x7f0c005d;
     }
     public static final class string {
         public static final int status_bar_notification_info_overflow = 0x7f0f0095;
---
+++ app/src/main/java/com/app/luberack/Fragments/Activity_Vehicles_Details.java
@@ -0,0 +1,18 @@
+package com.app.luberack.Fragments;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.app.luberack.R;
+
+/**
+ * Created by ahmad on 5/10/2018.
+ */
+
+public class Activity_Vehicles_Details extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vehicles_details);
+    }
+}
